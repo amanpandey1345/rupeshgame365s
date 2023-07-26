865979af0e3c10d1f6dc7e7e195775b25968ae35
amanpandey1345 <70140546+amanpandey1345@users.noreply.github.com>
2023-07-26 14:06:54 +0530

    adfasdfasdf

---
+++ src/app/not-found.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '@/designs/service-center.css'
+const NotFound = () => {
+  return (
+    <>
+     <div id='my-profile'>
+        <center><h3 className='text-red-700'> Sorry!!! page Not Found</h3> <br />
+        </center>
+      </div>
+    </>
+  )
+}
+
+export default NotFound
