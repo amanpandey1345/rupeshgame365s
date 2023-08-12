1e6d959d6079e1fa0122e837e0ca346983e72e8b
amanpandey1345 <70140546+amanpandey1345@users.noreply.github.com>
2023-08-12 14:04:54 +0530

    Update page.jsx

--- src/app/dashboard/dragon-tiger/page.jsx
+++ src/app/dashboard/dragon-tiger/page.jsx
@@ -16,8 +16,22 @@ const DragonTiger = () => {
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
   const { mutate} = useSWR(`/api/auth/me`, fetcher);
   const router = useRouter();
-  const [amount, setAmount] = useState(0)
+  const [amountT, handleamountT] = useState(0)
   const [betOn, setBetOn] = useState()
+  const [amountTA, handleamountTA] = useState(0)
+  const [amountD, handleamountD] = useState(0)
+  
+  const handleamount = (a) => {
+    if (betOn ==="tiger"){
+      handleamountT(amountT+a)
+    }
+    if (betOn ==="tie"){
+      handleamountTA(amountTA+a)
+    }
+    if (betOn ==="dragon"){
+      handleamountD(amountD+a)
+    }
+  }
   // const [error,setError] = useStart(null)
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -25,32 +39,81 @@ const DragonTiger = () => {
 
     
     try {
-      const res = await fetch("/api/bet", {
+      if (amountT){
+      const res0 = await fetch("/api/bet", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          BetOn:betOn,
-          BetAmount:amount
+          BetOn:"tiger",
+          BetAmount:amountT
         }),
-      });
-      const data = await res.json()
-      console.log(res);
-      console.log(data);
-      if (data.success === true) {
+      })};
+      if (amountD){
+      const res1 = await fetch("/api/bet", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          BetOn:"dragon",
+          BetAmount:amountD
+        }),
+      })};
+      if (amountTA){
+      const res2 = await fetch("/api/bet", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          BetOn:"tie",
+          BetAmount:amountTA
+        }),
+      })};
+
+      const data0 = await res0.json()
+      const data1 = await res1.json()
+      const data2 = await res2.json()
+      if (data0.success === true) {
         // redirect("/")
-        toast.success(data.message);
+        toast.success(data0.message);
         mutate()
         router.push("/")
         // setloading(false);
 
       }
-      if (data.success === false) {
+      if (data0.success === false) {
         // setloading(false);
-        console.log(data);
         // setError(data.message);
-        toast.error(data.message);
+        toast.error(data0.message);
+      }
+      if (data1.success === true) {
+        // redirect("/")
+        toast.success(data1.message);
+        mutate()
+        router.push("/")
+        // setloading(false);
+
+      }
+      if (data1.success === false) {
+        // setloading(false);
+        // setError(data.message);
+        toast.error(data1.message);
+      }
+      if (data2.success === true) {
+        // redirect("/")
+        toast.success(data2.message);
+        mutate()
+        router.push("/")
+        // setloading(false);
+
+      }
+      if (data2.success === false) {
+        // setloading(false);
+        // setError(data.message);
+        toast.error(data2.message);
       }
     } catch (err) {
       // setError(err.message);
@@ -66,55 +129,47 @@ const DragonTiger = () => {
       <Results />
       <Music />
       <section id='dragon-tiger'>
-        {
-          betOn === "dragon" || !betOn ?
-       
+        
         <div id='dragon' onClick={()=>setBetOn("dragon")} >
-          <h5>{amount ?`₹ ${amount}`:"₹ 000.00"}</h5>
+          <h5>{amountD ?`₹ ${amountD}`:"₹ 000.00"}</h5>
           <Image src={dragon} width={100} height={100} alt='dragon-img' />
           <h4>Dragon</h4>
-        </div>:null
-        }
+        </div>
+        
 
-{
-            betOn === "tie" || !betOn?
+
         <div id='tie' onClick={()=>setBetOn("tie")} >
-          <h5>{amount ?`₹ ${amount}`:"₹ 000.00"}</h5>
+          <h5>{amountTA ?`₹ ${amountTA}`:"₹ 000.00"}</h5>
           <Image src={tie} width={100} height={100} alt='tie-img' />
           <h4>Tie</h4>
-        </div>:null
-}
+        </div>
 
-{
-            betOn === "tiger" || !betOn ?
+
+
         <div id='tiger' onClick={()=>setBetOn("tiger")} >
-          <h5>{amount ?`₹ ${amount}`:"₹ 000.00"} </h5>
+          <h5>{amountT ?`₹ ${amountT}`:"₹ 000.00"} </h5>
           <Image src={tiger} width={100} height={100} alt='tiger-img' />
           <h4>Tiger</h4>
-        </div>:null
-}
-      </section>
-      {
-        betOn && 
+        </div>
 
-      <section id='bet-coin'>
-          <span onClick={()=>setAmount(amount+10)}>10</span>
-          <span onClick={()=>setAmount(amount+20)}>20</span>
-          <span onClick={()=>setAmount(amount+50)}>50</span>
-          <span onClick={()=>setAmount(amount+100)}>100</span>
-          <span onClick={()=>setAmount(amount+200)}>200</span>
-          <span onClick={()=>setAmount(amount+500)}>500</span>
-          <span onClick={()=>setAmount(amount+1000)}>1000</span>
-          <span onClick={()=>setAmount(amount+2000)}>2000</span>
       </section>
-      }
-      {
-        betOn && 
+     
+      <section id='bet-coin'>
+          <span onClick={()=>handleamount(10)}>10</span>
+          <span onClick={()=>handleamount(20)}>20</span>
+          <span onClick={()=>handleamount(50)}>50</span>
+          <span onClick={()=>handleamount(100)}>100</span>
+          <span onClick={()=>handleamount(200)}>200</span>
+          <span onClick={()=>handleamount(500)}>500</span>
+          <span onClick={()=>handleamount(1000)}>1000</span>
+          <span onClick={()=>handleamount(2000)}>2000</span>
+      </section>
+      
+      
             <section id='bet-btn'>
             <span onClick={(e)=>handleSubmit(e)}>Bet</span>
        
         </section>
-            }
                  <Toaster />
     </>
   )
